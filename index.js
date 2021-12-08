49b694d9cef24419c6b0b17cf463817e78a44216
JCernei <jonnycernei@gmail.com>
2021-12-08 22:21:41 +0200

    Add 3 figures

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 "use strict";
 
+class MyFigure {
+  constructor(vertices, uniforms, translation, xRotation, yRotation, zRotation) {
+    this.vertices = vertices;
+    this.uniforms = uniforms;
+    this.translation = translation;
+    this.xRotation = xRotation;
+    this.yRotation = yRotation;
+    this.zRotation = zRotation;
+  }
+}
+
 function main() {
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
@@ -14,12 +25,11 @@ function main() {
   var program = programInfo.program;
 
   //-------------------------------------------------
-  // creates buffers with position, normal, texcoord, and vertex color
-  // data for primitives by calling gl.createBuffer, gl.bindBuffer,
-  // and gl.bufferData
-  // const sphereBufferInfo = primitives.createSphereWithVertexColorsBufferInfo(gl, 10, 12, 6);
-  var cubeVertices = primitives.createCubeVertices(20);
+  const CUBE_SHAPE = 0;
+  const CONE_SHAPE = 1;
+  const SPHERE_SHAPE = 2;
 
+  var cubeVertices = primitives.createCubeVertices(40);
   cubeVertices = primitives.deindexVertices(cubeVertices);
   cubeVertices = primitives.makeRandomVertexColors(cubeVertices, {
     vertsPerColor: 6,
@@ -27,16 +37,17 @@ function main() {
       return channel < 3 ? ((128 + Math.random() * 128) | 0) : 255;
     },
   });
-  const cubeBufferInfo = primitives.createCubeWithVertexColorsBufferInfo(gl, 20);
-  // const coneBufferInfo   = primitives.createTruncatedConeWithVertexColorsBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
 
-  // console.log(cubeVertices.normal);
-  // console.log(cubeBufferInfo);
+  var coneVertices = primitives.createTruncatedConeVertices(20, 0, 40, 12, 1, true, false);
+  coneVertices = primitives.deindexVertices(coneVertices);
+  coneVertices = primitives.makeRandomVertexColors(coneVertices, {
+    vertsPerColor: 6,
+    rand: function (ndx, channel) {
+      return channel < 3 ? ((128 + Math.random() * 128) | 0) : 255;
+    },
+  });
 
-
-
-  var sphereVertices = primitives.createSphereVertices(20, 10, 16);
-
+  var sphereVertices = primitives.createSphereVertices(20, 20, 20);
   sphereVertices = primitives.deindexVertices(sphereVertices);
   sphereVertices = primitives.makeRandomVertexColors(sphereVertices, {
     vertsPerColor: 6,
@@ -44,58 +55,33 @@ function main() {
       return channel < 3 ? ((128 + Math.random() * 128) | 0) : 255;
     },
   });
-  const sphereBufferInfo = primitives.createSphereWithVertexColorsBufferInfo(gl, 20, 12, 6);
-  // const coneBufferInfo   = primitives.createTruncatedConeWithVertexColorsBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
 
-  console.log(sphereVertices.normal);
-  console.log(sphereBufferInfo);
+  var shapes = [
+    cubeVertices,
+    coneVertices,
+    sphereVertices,
+  ];
 
-
-  // Uniforms for each object.
-  var sphereUniforms = {
-    u_colorMult: [0.5, 1, 0.5, 1],
-    u_worldViewProjection: m4.identity(),
-  };
-  var cubeUniforms = {
-    u_colorMult: [1, 0.5, 0.5, 1],
-    u_worldViewProjection: m4.identity(),
-  };
-  // var coneUniforms = {
-  //   u_colorMult: [0.5, 0.5, 1, 1],
-  //   u_worldViewProjection: m4.identity(),
-  // };
-  var sphereTranslation = [0, 0, 0];
-  var cubeTranslation = [-40, 0, 0];
-  // var coneTranslation   = [ 40, 0, 0];
-
-  // var objectsToDraw = [
-  //   {
-  //     programInfo: programInfo,
-  //     bufferInfo: sphereBufferInfo,
-  //     uniforms: sphereUniforms,
-  //   },
-  //   {
-  //     programInfo: programInfo,
-  //     bufferInfo: cubeBufferInfo,
-  //     uniforms: cubeUniforms,
-  //   },
-  //   {
-  //     programInfo: programInfo,
-  //     bufferInfo: coneBufferInfo,
-  //     uniforms: coneUniforms,
-  //   },
-  // ];
-
-  function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation) {
-    var matrix = m4.translate(viewProjectionMatrix,
-      translation[0],
-      translation[1],
-      translation[2]);
-    matrix = m4.xRotate(matrix, xRotation);
-    return m4.yRotate(matrix, yRotation);
-  }
-  requestAnimationFrame(drawScene);
-  //-------------------------------------------------
+  var fs = [
+    new MyFigure(shapes[SPHERE_SHAPE], { u_colorMult: [0.2, 1, 0.2, 1], u_matrix: m4.identity() },
+      [70, 0, 0],
+      rand(0.8, 1.2),
+      rand(0.8, 1.2),
+      rand(0.8, 1.2)
+    ),
+    new MyFigure(shapes[CUBE_SHAPE], { u_colorMult: [1, 0.2, 0.2, 1], u_matrix: m4.identity() },
+      [0, 0, 0],
+      rand(0.8, 1.2),
+      rand(0.8, 1.2),
+      rand(0.8, 1.2)
+    ),
+    new MyFigure(shapes[CONE_SHAPE], { u_colorMult: [0.2, 0.2, 1, 1], u_matrix: m4.identity() },
+      [-70, 0, 0],
+      rand(0.8, 1.2),
+      rand(0.8, 1.2),
+      rand(0.8, 1.2)
+    ),
+  ]
 
   // lookup uniforms
   var worldViewProjectionLocation = gl.getUniformLocation(program, "u_worldViewProjection");
@@ -117,21 +103,19 @@ function main() {
   var positionLocation = gl.getAttribLocation(program, "a_position");
   var normalLocation = gl.getAttribLocation(program, "a_normal");
 
-  // Create a buffer to put positions in
+  // Create buffers to put positions and normals in
   var positionBuffer = gl.createBuffer();
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  // Put geometry data into buffer
-  // setGeometry(gl, cubeVertices);
-  setGeometry(gl, sphereVertices);
-
-  // Create a buffer to put normals in
   var normalBuffer = gl.createBuffer();
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // Put geometry data into buffer
+  // setGeometry(gl, f.vertices);
+
+
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
-  gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+  // gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
   // Put normals data into buffer
-  // setNormals(gl, cubeVertices);
-  setNormals(gl, sphereVertices);
+  // setNormals(gl, f.vertices);
 
 
   function radToDeg(r) {
@@ -161,142 +145,105 @@ function main() {
     shininess = ui.value;
     // drawScene();
   }
+  console.log(fs)
 
   // Draw the scene.
   function drawScene(time) {
     time *= 0.0005;
     webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
     // Tell WebGL how to convert from clip space to pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
     // Clear the canvas AND the depth buffer.
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
     // Turn on culling. By default backfacing triangles
     // will be culled.
     gl.enable(gl.CULL_FACE);
-
     // Enable the depth buffer
     gl.enable(gl.DEPTH_TEST);
-
     // Tell it to use our program (pair of shaders)
     gl.useProgram(program);
-
-    // Turn on the position attribute
-    gl.enableVertexAttribArray(positionLocation);
-
-    // Bind the position buffer.
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-    var size = 3;          // 3 components per iteration
-    var type = gl.FLOAT;   // the data is 32bit floats
-    var normalize = false; // don't normalize the data
-    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-    var offset = 0;        // start at the beginning of the buffer
-    gl.vertexAttribPointer(
-      positionLocation, size, type, normalize, stride, offset);
-
-    // Turn on the normal attribute
-    gl.enableVertexAttribArray(normalLocation);
-
-    // Bind the normal buffer.
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-
-    // Tell the attribute how to get data out of normalBuffer (ARRAY_BUFFER)
-    var size = 3;          // 3 components per iteration
-    var type = gl.FLOAT;   // the data is 32bit floating point values
-    var normalize = false; // normalize the data (convert from 0-255 to 0-1)
-    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-    var offset = 0;        // start at the beginning of the buffer
-    gl.vertexAttribPointer(
-      normalLocation, size, type, normalize, stride, offset);
 
     // Compute the projection matrix
     var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     var zNear = 1;
     var zFar = 2000;
     var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
-
     // Compute the camera's matrix
     var camera = [0, 0, 100];
     var target = [0, 0, 0];
     var up = [0, 1, 0];
     var cameraMatrix = m4.lookAt(camera, target, up);
-
     // Make a view matrix from the camera matrix.
     var viewMatrix = m4.inverse(cameraMatrix);
-
     // Compute a view projection matrix
     var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
 
-    // Draw a F at the origin
-    var worldMatrix = m4.yRotation(fRotationRadians * time);
-
-    // Multiply the matrices.
-    var worldViewProjectionMatrix = m4.multiply(viewProjectionMatrix, worldMatrix);
-    var worldInverseMatrix = m4.inverse(worldMatrix);
-    var worldInverseTransposeMatrix = m4.transpose(worldInverseMatrix);
-
-    // Set the matrices
-    gl.uniformMatrix4fv(worldViewProjectionLocation, false, worldViewProjectionMatrix);
-    gl.uniformMatrix4fv(worldInverseTransposeLocation, false, worldInverseTransposeMatrix);
-    gl.uniformMatrix4fv(worldLocation, false, worldMatrix);
-
-    // Set the color to use
-    gl.uniform4fv(colorLocation, [0.2, 1, 0.2, 1]); // green
-
     // set the light position
-    gl.uniform3fv(lightWorldPositionLocation, [0, 0, 30]);
-
+    gl.uniform3fv(lightWorldPositionLocation, [0, 0, 100]);
     // set the camera/view position
     gl.uniform3fv(viewWorldPositionLocation, camera);
-
     // set the shininess
     gl.uniform1f(shininessLocation, shininess);
-
     // set the light color
     gl.uniform3fv(lightColorLocation, m4.normalize([1, 1, 1]));  // red light
-
     // set the specular color
     gl.uniform3fv(specularColorLocation, m4.normalize([0.2, 0.2, 1]));  // red light
 
-    // Draw the geometry.
-    var primitiveType = gl.TRIANGLES;
-    var offset = 0;
-    // var count = 36; //cube
-    var count = 1000;  //sphere
-    gl.drawArrays(primitiveType, offset, count);
+    for (var f of fs) {
+      // Turn on the position attribute
+      gl.enableVertexAttribArray(positionLocation);
+      // Bind the position buffer.
+      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+      setGeometry(gl, f.vertices);
+      gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+      // Turn on the normal attribute
+      gl.enableVertexAttribArray(normalLocation);
+      // Bind the normal buffer.
+      gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+      setNormals(gl, f.vertices);
+      gl.vertexAttribPointer(normalLocation, 3, gl.FLOAT, false, 0, 0);
+
+      // Draw
+      var matrix = m4.translate(m4.identity(),
+        f.translation[0],
+        f.translation[1],
+        f.translation[2]);
+      matrix = m4.xRotate(matrix, f.xRotation + time);
+      matrix = m4.yRotate(matrix, f.yRotation + time);
+      matrix = m4.zRotate(matrix, f.zRotation + time);
+      f.uniforms.u_matrix = matrix;
+
+      var worldMatrix = f.uniforms.u_matrix; //m4.yRotation(fRotationRadians * time);
+      // Set the color to use
+      gl.uniform4fv(colorLocation, f.uniforms.u_colorMult);
+      // Multiply the matrices.
+      var worldViewProjectionMatrix = m4.multiply(viewProjectionMatrix, worldMatrix);
+      var worldInverseMatrix = m4.inverse(worldMatrix);
+      var worldInverseTransposeMatrix = m4.transpose(worldInverseMatrix);
+      // Set the matrices
+      gl.uniformMatrix4fv(worldViewProjectionLocation, false, worldViewProjectionMatrix);
+      gl.uniformMatrix4fv(worldInverseTransposeLocation, false, worldInverseTransposeMatrix);
+      gl.uniformMatrix4fv(worldLocation, false, worldMatrix);
+
+      gl.drawArrays(gl.TRIANGLES, 0, f.vertices.position.numElements);
+    }
+
     requestAnimationFrame(drawScene);
   }
 }
 
 // Fill the buffer with the values that define a letter 'F'.
-function setGeometry(gl, vertices) {
-  var positions = vertices.position
-  // Center the F around the origin and Flip it around. We do this because
-  // we're in 3D now with and +Y is up where as before when we started with 2D
-  // we had +Y as down.
-
-  // We could do by changing all the values above but I'm lazy.
-  // We could also do it with a matrix at draw time but you should
-  // never do stuff at draw time if you can do it at init time.
-  // var matrix = m4.xRotation(Math.PI);
-  // matrix = m4.translate(matrix, 0, 0, 0 );
-
-  // for (var ii = 0; ii < positions.length; ii += 3) {
-  //   var vector = m4.transformPoint(matrix, [positions[ii + 0], positions[ii + 1], positions[ii + 2], 1]);
-  //   positions[ii + 0] = vector[0];
-  //   positions[ii + 1] = vector[1];
-  //   positions[ii + 2] = vector[2];
-  // }
-
-  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+function setGeometry(gl, gVertices) {
+  gl.bufferData(gl.ARRAY_BUFFER, gVertices.position, gl.STATIC_DRAW);
 }
 
-function setNormals(gl, vertices) {
-  gl.bufferData(gl.ARRAY_BUFFER, vertices.normal, gl.STATIC_DRAW);
+function setNormals(gl, nVertices) {
+  gl.bufferData(gl.ARRAY_BUFFER, nVertices.normal, gl.STATIC_DRAW);
+}
+
+function rand(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
 main();
